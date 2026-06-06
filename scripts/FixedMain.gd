e4a6b318a9bcca6d44d913b6c4d3aea9b64a4d46
gptaccselivonchik18-collab <gptaccselivonchik18@gmail.com>
2026-06-06 09:11:30 +0300

    Replace broken startup script with stable playable build

--- scripts/FixedMain.gd
+++ scripts/FixedMain.gd
@@ -1,25 +1,22 @@
 extends Node
 
 var ui: Control
-var world: Node3D
 var car: Node3D
-var cam: Camera3D
-var hud: Label
-var mode := "menu"
 var speed := 0.0
+var steer := 0.0
 var nitro := 100.0
 var t := 0.0
 var score := 0
-var max_kmh := 0.0
-var drift := 0
-var cp := 0
-var cps := [Vector3(0,0,-70), Vector3(32,0,-145), Vector3(-28,0,-225), Vector3(18,0,-320), Vector3(0,0,-430)]
+var racing := false
 var left := false
 var right := false
 var gas := false
 var brake := false
-var hand := false
 var boost := false
 
 func _ready() -> void:
-	Display
+	DisplayServer.screen_set_orientation(DisplayServer.SCREEN_SENSOR_LANDSCAPE)
+	Engine.max_fps = 60
+	show_menu()
+
+func clear()
